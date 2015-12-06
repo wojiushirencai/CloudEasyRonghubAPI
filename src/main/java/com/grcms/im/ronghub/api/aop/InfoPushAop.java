629e21b417532640f3a5dee02a74f30c61ee7928
djiepeng <djiepeng@126.com>
2015-12-06 22:52:54 +0800

    Add aop to intercept add info action and use push framework push to all devices

---
+++ src/main/java/com/grcms/im/ronghub/api/aop/InfoPushAop.java
@@ -0,0 +1,31 @@
+package com.grcms.im.ronghub.api.aop;
+
+import com.grcms.common.util.CommonUtility;
+import com.grcms.content.core.domain.CMSInfo;
+import com.grcms.core.response.JsonResponse;
+import org.aopalliance.intercept.Joinpoint;
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+/**
+ * Created by Administrator on 2015/12/6.
+ * if
+ */
+@Aspect
+public class InfoPushAop {
+    private static final Logger logger = Logger.getLogger(InfoPushAop.class);
+    @AfterReturning(pointcut="execution(* com.grcms.content.core.controller.ManagementInfoController.add(..))",returning="rvt")
+    public void log(JoinPoint jp, Object rvt) {
+        if(rvt instanceof JsonResponse) {
+            JsonResponse jres = (JsonResponse) rvt;
+            Object obj = jres.getResponse();
+            if(obj instanceof CMSInfo) {
+                CMSInfo info = (CMSInfo) obj;
+                logger.debug("==> Prepare to push info :title=" + info.getName() + ",content=" + info.getFields().get("content"));
+            }
+        }
+    }
+}
---
+++ src/main/java/com/grcms/im/ronghub/api/controller/AnnouncementController.java
@@ -0,0 +1,58 @@
+package com.grcms.im.ronghub.api.controller;
+
+import com.grcms.content.core.exception.ECCMSInfoException;
+import com.grcms.content.core.exception.ECCMSNodeException;
+import com.grcms.content.core.form.NodeForm;
+import com.grcms.content.core.service.CMSInfoService;
+import com.grcms.content.core.service.CMSNodeService;
+import com.grcms.content.core.util.ContentViewUtil;
+import com.grcms.core.exception.ECAuthException;
+import com.grcms.frontend.exception.ECMemberException;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 2015/11/15.
+ */
+
+@Controller
+@RequestMapping("")
+public class AnnouncementController {
+    /**
+     * Logger for this class
+     */
+    private static final Logger logger = Logger
+            .getLogger(AnnouncementController.class);
+    @Autowired
+    private CMSNodeService cmsNodeSerivce;
+    @Autowired
+    private CMSInfoService cmsInfoService;
+
+    /**
+     * @param request
+     * @param response
+     * @return
+     * @throws ECAuthException
+     * @throws ECMemberException
+     * @throws ECCMSNodeException
+     */
+    @RequestMapping(value = "/api/announcement", method = RequestMethod.GET)
+    public String polciy(HttpServletRequest request, HttpServletResponse response
+            , @ModelAttribute("form") NodeForm form) throws ECAuthException, ECMemberException, ECCMSNodeException {
+        return ContentViewUtil.findNodeView(cmsNodeSerivce, request, "announcement", false);
+    }
+
+    @RequestMapping(value = "/announcement/{infoId}.do", method = RequestMethod.GET)
+    public String polciyDetail(HttpServletRequest request, HttpServletResponse response, @PathVariable String infoId) throws ECAuthException, ECMemberException, ECCMSNodeException, ECCMSInfoException {
+        return ContentViewUtil.findInfoView(cmsNodeSerivce, cmsInfoService, request, "announcement", infoId, false);
+    }
+}
+
