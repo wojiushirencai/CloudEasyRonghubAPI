d972afbc0756d2aa4355d84e115474af7a1a6c48
djiepeng <jiepeng@globalroam.com>
2015-12-17 23:27:47 +0800

    add backend daily

--- src/main/java/com/grcms/im/ronghub/api/controller/DailyBackendController.java
+++ src/main/java/com/grcms/im/ronghub/api/controller/DailyBackendController.java
@@ -1,18 +1,13 @@
 package com.grcms.im.ronghub.api.controller;
 
-import com.grcms.common.util.CommonUtility;
-import com.grcms.content.core.domain.CMSInfo;
 import com.grcms.content.core.exception.ECCMSInfoException;
 import com.grcms.content.core.exception.ECCMSNodeException;
 import com.grcms.core.exception.ECAuthException;
 import com.grcms.core.form.BasicForm;
 import com.grcms.core.response.JsonResponse;
-import com.grcms.core.util.CMSThemeUtil;
 import com.grcms.core.util.ForwardUtility;
-import com.grcms.frontend.domain.Member;
 import com.grcms.frontend.exception.ECMemberException;
 import com.grcms.frontend.service.MemberService;
-import com.grcms.im.ronghub.api.domain.Daily;
 import com.grcms.im.ronghub.api.service.DailyService;
 import com.grcms.im.ronghub.api.util.ApiUtil;
 import org.apache.log4j.Logger;
@@ -22,8 +17,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.util.Date;
 
 /**
  * Created by Administrator on 2015/11/15.
@@ -39,8 +32,6 @@ public class DailyBackendController {
             .getLogger(DailyBackendController.class);
     @Autowired
     private DailyService dailyService;
-    @Autowired
-    private MemberService memberService;
 
     /**
      * @param request
@@ -64,8 +55,10 @@ public class DailyBackendController {
 
     @RequestMapping(value = "/view", method = RequestMethod.GET)
     public String view(HttpServletRequest request, HttpServletResponse response
+            , @RequestParam String id
             , @ModelAttribute("form") BasicForm form) throws ECAuthException, ECMemberException, ECCMSNodeException {
-        return ForwardUtility.forwardAdminView("daily/data/data_json_daily");
+        request.setAttribute("id",id);
+        return ForwardUtility.forwardAdminView("daily/modal_daily_view");
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
@@ -79,29 +72,8 @@ public class DailyBackendController {
             return jres;
         }
 
-//        for(String id :ids) {
-//            CMSInfo info = cmsInfoService.findById(id);
-//            if (info.getNode().getInfoOpenStatic()) {
-//                String filepath = CMSThemeUtil.getPathAsLocal(new String[]{
-//                        request.getServletContext().getRealPath(""),
-//                        info.getUri()
-//                });
-//                File file = new File(filepath);
-//                file.delete();
-//            }
-//        }
-//        cmsInfoService.delete(ids);
+        dailyService.deleteByIds(ids);
         return jres;
-    }
-
-    @RequestMapping(value = "/daily/{dailyId}")
-    public String dailyDetail(HttpServletRequest request, HttpServletResponse response
-            , @PathVariable("dailyId") String dailyId
-            , @ModelAttribute("form") BasicForm form
-    ) throws ECAuthException, ECMemberException, ECCMSNodeException, ECCMSInfoException {
-        request.setAttribute("userId", ApiUtil.getRequestUserId(request));
-        request.setAttribute("token", ApiUtil.getRequestToken(request));
-        return ForwardUtility.forwardThemeView("daily_detail");
     }
 
 }
